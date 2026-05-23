3ed7bf95c286bf471f1fe59d057cc262ae7388b2
vinay22m9 <159008955+vinay22m9@users.noreply.github.com>
2026-05-23 22:08:58 +0530

    supabase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,9 +9,8 @@ export const environment = {
   appName: 'Fitness Coach',
   appVersion: '0.1.0',
 
-  supabaseUrl: 'https://YOUR_PROJECT.supabase.co',
-  supabaseAnonKey: 'YOUR_ANON_KEY',
-
+supabaseUrl: 'https://mcegbfqhodeenlcunuab.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jZWdiZnFob2RlZW5sY3VudWFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1MTkwMDksImV4cCI6MjA5NTA5NTAwOX0.3EVUowQC3ijcVRldI0zd7LpIVLdtrYrgGNEiL1eWni4',
   geminiApiKey: '',
   geminiModel: 'gemini-1.5-flash',
 
